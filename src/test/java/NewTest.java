a8c87f40881f2de3075fd53f972be98015fca8c5
Dharma062 <Hanuma.Phani@iiitb.org>
2018-04-17 00:00:33 +0530

    new

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -1,26 +1,29 @@
 package example;
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
 
-import org.junit.Before;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
 
 
 public class NewTest {
-
     private WebDriver driver;
-
-    @Before
-    public void setUp() {
-        // Create a new instance of the html unit driver
-        driver = new HtmlUnitDriver();
-
-        //Navigate to desired web page
+    @Test
+    public void testEasy() {
         driver.get("http://localhost:8011/login");
+        String title = driver.getTitle();
+        Assert.assertTrue(title.contains("Demo Guru99 Page"));
+    }
+    @BeforeTest
+    public void beforeTest() {
+        driver = new FirefoxDriver();
+    }
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
     }
 }
 
