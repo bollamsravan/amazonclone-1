768aa60f3b40510b6b60e6b964b926f009574438
Dharma062 <Hanuma.Phani@iiitb.org>
2018-04-16 18:54:28 +0530

    new

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -1,3 +1,25 @@
+//package example;
+//
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.testng.Assert;
+//import org.testng.annotations.Test;
+//import org.testng.annotations.BeforeTest;
+//import org.testng.annotations.AfterTest;
+//
+//public class NewTest {
+//    public static void main(String[] args){
+//        FirefoxDriver driver=new FirefoxDriver();
+//        driver.get("http://localhost:8011/login");
+//        WebElement element=driver.findElement(By.xpath("//input[@name='email']"));
+//        element.sendKeys("dharmaraja.vandanapu@gmail.com");
+//        WebElement element=driver.findElement(By.xpath("//input[@name='psword']"));
+//        element.sendKeys("dharma@99");
+//        WebElement button=driver.findElement(By.xpath("//input[@name='btn-login']"));
+//        button.click();
+//    }
+//}
 package example;
 
 import org.openqa.selenium.By;
@@ -8,14 +30,19 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 public class NewTest {
-    public static void main(String[] args){
-        FirefoxDriver driver=new FirefoxDriver();
-        driver.get("http://localhost:8011/login");
-        WebElement element=driver.findElement(By.xpath("//input[@name='email']"));
-        element.sendKeys("dharmaraja.vandanapu@gmail.com");
-        WebElement element=driver.findElement(By.xpath("//input[@name='psword']"));
-        element.sendKeys("dharma@99");
-        WebElement button=driver.findElement(By.xpath("//input[@name='btn-login']"));
-        button.click();
+    private WebDriver driver;
+    @Test
+    public void testEasy() {
+        driver.get("http://demo.guru99.com/test/guru99home/");
+        String title = driver.getTitle();
+        Assert.assertTrue(title.contains("Demo Guru99 Page"));
     }
-}
+    @BeforeTest
+    public void beforeTest() {
+        driver = new FirefoxDriver();
+    }
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+    }
+}	
