600e330c6d516e59ee7dcbe5b92b2c02c33b4cce
Dharma062 <Hanuma.Phani@iiitb.org>
2018-04-16 19:12:10 +0530

    new

--- src/test/java/NewTest.java
+++
@@ -1,8 +0,0 @@
-@Test
-public void anExampleTest() {
-
-        // Check that the add() method of a calculator returns 4
-        // when it is asked to add 2 and 2
-
-        Assert.assertEquals(4, calculator.add(2,2));
-        }
