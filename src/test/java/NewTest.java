5efe363a874ca2eb8707c091029e4feedaab2ae3
bollamsravan1603 <bollamsravan1603@gmail.com>
2018-04-17 12:06:39 +0530

    new

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -7,30 +7,20 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.WebDriver;
 
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Test;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 
 
 public class NewTest {
 
-    private WebDriver driver;
     @Test
     public void testEasy() {
-        System.setProperty("webdriver.gecko.driver", "/home/dharma_vandanapu/seleniumGecko/geckodriver");
+        System.setProperty("webdriver.gecko.driver", "/home/shravan/SeleniumGecko/geckodriver");
 
         //Now you can Initialize marionette driver to launch firefox
         WebDriver driver = new FirefoxDriver();
-        driver.get("http://localhost:8011/login");
-        String title = driver.getTitle();
-        Assert.assertTrue(title.contains("Demo Guru99 Page"));
-    }
-    @BeforeTest
-    public void beforeTest() {
-        driver = new FirefoxDriver();
-    }
-    @AfterTest
-    public void afterTest() {
+        driver.get("http://localhost:8083/login");
         driver.quit();
     }
 }
